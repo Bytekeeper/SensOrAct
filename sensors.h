a86af45c1b78594b3678fc42a3511d1b72442997
Bytekeeper <therealdante@gmail.com>
2015-04-13 21:18:54 +0200

    Added Message type

--- sensors.h
+++ sensors.h
@@ -1,6 +1,13 @@
 #ifndef _SENSORS_H_
 #define _SENSORS_H_
 
+#include <Arduino.h>
+
 const uint8_t TYPE_RGB_LED = 0;
+
+typedef struct {
+    uint8_t type;
+    uint8_t payload[15];
+} Message;
 
 #endif
