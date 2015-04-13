344c60785430bb0e29cfd61018a5ed723572c7a3
Bytekeeper <therealdante@gmail.com>
2015-04-13 21:23:03 +0200

    Added rgb fade command

--- sensors.h
+++ sensors.h
@@ -10,4 +10,14 @@ typedef struct {
     uint8_t payload[15];
 } Message;
 
+const uint8_t RGB_LED_MODE_FADE = 0;
+
+typedef struct {
+    uint8_t type;
+    uint8_t mode;
+    uint16_t src_r, src_g, src_b;
+    uint16_t trg_r, trg_g, trg_b;
+
+} CmdRGBLedFade;
+
 #endif
